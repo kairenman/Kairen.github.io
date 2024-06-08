ad2bf94ec083baf86010a250c2fdd06d9437b446
kairenman <154108634+kairenman@users.noreply.github.com>
2024-06-08 18:50:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,23 @@ function clickLdisike2(){
   countdisLike2.textContent = totaldisLikes.toString()
 }
 btndisLike2.addEventListener("click",clickdisLike2)
+const btnDisLike1 = document.getElementById("btnDisLike1")
+const countDisLike1 = document.getElementById("countDisLike1")
+
+function clickDisLike1(){
+  let totalDisLikes = parseInt(countDisLike1.value) + 1
+  countDisLike1.textContent = totalDisLikes.toString()
+}  
+btnDisLike1.addEventListener("click",clickDisLike1)
+
+const btnDisLike2 = document.getElementById("btnDisLike2")
+const countDisLike2 = document.getElementById("countDisLike2")
+
+function clickDisLike2(){
+  let totalDisLikes = parseInt(countDisLike2.value) + 1
+  countDisLike2.textContent = totalDisLikes.toString()
+}  
+btnDisLike2.addEventListener("click",clickDisLike2)
 
 const submit = document.getElementById("submit")
 
